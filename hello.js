a36b6031aadef4a3ece5dc275ed12297da0ff093
kt2284 <78666597+kt2284@users.noreply.github.com>
2025-12-31 10:41:40 +0900

    Replace HTML structure with a JavaScript alert

--- hello.js
+++ hello.js
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>TestJS via jsDelivr</title>
-</head>
-<body>
-  <script src="https://cdn.jsdelivr.net/gh/kt2284/TestJS/hello.js"></script>
-</body>
-</html>
+alert('Hello, world!');
